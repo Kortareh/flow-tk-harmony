6dbbd893169186810ae8f7c18f8efd713355b487
Coby <78200887+Kortareh@users.noreply.github.com>
2026-04-27 16:14:00 +1000

    Refactor MenuGenerator.show and add logging
    Unindent show() so it becomes a module-level function (previously defined as MenuGenerator.show). Enhance debug logging to include incoming position and cursor, add a pre-popup debug statement, and log popup visibility and action count after showing. Also add an extra qApp.processEvents() after popup to ensure UI updates; retains activateWindow/raise_/popup sequence.

--- python/tk_harmony/menu_generation.py
+++ python/tk_harmony/menu_generation.py
@@ -48,18 +48,24 @@ class MenuGenerator(object):
     def hide(self):
         self.menu_handle.hide()
 
-    def show(self, pos=None):
-        qApp = QtGui.QApplication.instance()
+def show(self, pos=None):
+    qApp = QtGui.QApplication.instance()
+    cursor_pos = QtGui.QCursor.pos()
 
-        cursor_pos = QtGui.QCursor.pos()
-        self.logger.debug("Menu show requested. incoming_pos=%s cursor_pos=%s" % (pos, cursor_pos))
+    self.logger.debug("MenuGenerator.show ENTER pos=%s cursor=%s" % (pos, cursor_pos))
 
-        self.menu_handle.activateWindow()
-        self.menu_handle.raise_()
+    self.menu_handle.activateWindow()
+    self.menu_handle.raise_()
 
-        qApp.processEvents()
+    qApp.processEvents()
 
-        self.menu_handle.popup(cursor_pos)
+    self.logger.debug("MenuGenerator.show BEFORE popup")
+    self.menu_handle.popup(cursor_pos)
+    qApp.processEvents()
+    self.logger.debug("MenuGenerator.show AFTER popup visible=%s actions=%s" % (
+        self.menu_handle.isVisible(),
+        len(self.menu_handle.actions())
+    ))
 
     def create_menu(self, disabled=False):
         """
