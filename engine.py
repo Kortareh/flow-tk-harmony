70f7a12e0f66085b9bb80481dfcf80b68d786d3e
Coby <78200887+Kortareh@users.noreply.github.com>
2026-04-27 15:35:56 +1000

    Simplify on_show_menu; remove position handling
    Remove debug logging and parsing of clickedPosition in on_show_menu. The handler no longer computes a menu_position from kwargs and now simply calls display_menu() with no pos argument. This cleans up unused code and avoids relying on event positional data when showing the menu.

--- engine.py
+++ engine.py
@@ -736,15 +736,7 @@ class HarmonyEngine(Engine):
         # the engine config says so. We do it here as we try to avoid
         # deffer querying the dcc app to the last minute, to give it time
         # to initialize.
-        self.logger.debug("on_show_menu called.")
-
-        menu_position = None
-        clicked_info = kwargs.get("clickedPosition")
-        if clicked_info:
-            menu_position = [clicked_info["x"], clicked_info["y"]]
-
-        self.logger.debug("on_show_menu | menu_position %s" % menu_position)
-        self.display_menu(pos=menu_position)
+        self.display_menu()
 
     def on_new_project_created(self, **kwargs):
         path = kwargs.get("path")
